0bc57143402303eca9aaf86faf24a6bd312e5a70
Khuongh <60898006+Khuongh@users.noreply.github.com>
2022-03-25 16:56:00 +0100

    s

--- applications/First_try_or_not/src/custom_files/button_leds.h
+++
@@ -1,8 +0,0 @@
-#include <zephyr.h>
-#include <logging/log.h>
-#include <dk_buttons_and_leds.h>
-#include <drivers/gpio.h>
-
-
-
-void configure_dk_buttons_leds();
--- applications/observer_test_1/src/custom_files/kalman_filter.h
+++
@@ -1,7 +0,0 @@
-#include <zephyr/types.h>
-#include <stddef.h>
-#include <sys/printk.h>
-#include <sys/util.h>
-
-
-int8_t KALMAN(int8_t U);
