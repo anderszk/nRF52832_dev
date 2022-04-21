987357d29394744a011d9df94218842b38cc4d7d
Håvard Olai Kopperstad <52386166+haavardok@users.noreply.github.com>
2022-04-21 14:51:14 +0200

    Update encoder.c
    Printar noko til nrf_terminal, men gir ikkje heilt meining kva det er.

--- code/src/Encoder_experimental/Encoder_QDEC_improvement/encoder.c
+++ code/src/Encoder_experimental/Encoder_QDEC_improvement/encoder.c
@@ -1,86 +1,94 @@
 #include <zephyr.h>
 #include <sys/printk.h>
+#include <logging/log.h>
 #include <devicetree.h>
 #include <drivers/gpio.h>
 #include <nrfx_qdec.h>
-#include <nrfx_gpiote.h>
-
-const uint32_t pin_a = 3;
-const uint32_t pin_b = 4;
-//const uint32_t pin_a = 32+1; //Denne ga same feilen som å bruke pin 3 og 4
-//const uint32_t pin_b = 32+2;
-const uint32_t pin_led = 0xFFFFFFFF;
 
 
-// nrfx_qdec_config_t qdec_config = NRFX_QDEC_DEFAULT_CONFIG(pin_a, pin_b, pin_led);
+const uint32_t pin_a = 26;
+const uint32_t pin_b = 27;
 
+// static volatile int16_t acc_data = 0;
+// static volatile uint16_t accdbl_data = 0;
 
-volatile int16_t acc_data = 0;
-volatile int16_t accdbl_data = 0;
-
+static volatile uint16_t m_accdblread;
+static volatile int16_t m_accread;
+static volatile int16_t m_value=0;
+static volatile int16_t m_last_value=0;
 
 
 static void qdec_nrfx_event_handler(nrfx_qdec_event_t event)
 {
-    //printk("Enetered qdec_nrfx_event_handler \n");
-	switch (event.type) {
-    case NRF_QDEC_EVENT_SAMPLERDY:
-        printk("SAMPLERDY interrupt \n");
-		//nrfx_qdec_accumulators_read(acc_data, accdbl_data);
-		//printk(event.data.report.acc);
-		//printk(event.data.report.accdbl);
-        break;
+   if (event.type == NRF_QDEC_EVENT_REPORTRDY) {
+    //    acc_data = event.data.report.acc;
+    //    accdbl_data = event.data.report.accdbl;
+    //     printk("Data1: %u\n", acc_data);
+    //     //printk("Data2: %u\n", accdbl_data);
 
-	case NRF_QDEC_EVENT_REPORTRDY:
-        printk("REPORTRDY event handled");
-		acc_data = event.data.report.acc;
-		//printk(acc_data);
+        m_accdblread = event.data.report.accdbl;
+        m_accread = event.data.report.acc;
+        if (m_accdblread==0) {
+        	m_value += m_accread;
+	    }
+        if (m_value<0) {
+		m_value = 0;
+	    } else if (m_value>100) {
+		m_value=100;
+	    }
+        if (m_value != m_last_value) {
+            printk("ACC: %i\n", m_accread);
+            printk("ACCDBL: %i\n", m_accdblread);
 
-		//acc_data = event.data.report.acc;
-		//printk(acc_data);
-		//printk(event.data.report.accdbl);
-        break;
-
-    default:
-        //printk("unhandled event (0x%x) \n", event.type);
-        break;
-    }
+            // uart_printf("report dbl=%u acc=%d",m_accdblread,m_accread);
+            //     if (m_accread>0) {
+			// uart_printf("\x1B[1;32m"); // GREEN
+            //     } else {
+			// uart_printf("\x1B[1;31m"); // RED
+		    // }
+        	// uart_printf(" val=%d\n\r",m_value);
+		    // m_last_value = m_value;
+		    // uart_printf("\x1B[0m"); // DEFAULT color
+	    }
+        
+   }
+   
 }
 
 void main(void)
 
 {
-
-	nrfx_err_t err;
+    IRQ_CONNECT(QDEC_IRQn, 0, nrfx_isr, nrfx_qdec_irq_handler, 0);
+	irq_enable(QDEC_IRQn);
 
     nrfx_qdec_config_t qdec_config = 
     {                                                                
-        .reportper          = NRF_QDEC_REPORTPER_200,                 
-        .sampleper          = NRF_QDEC_SAMPLEPER_1024us,            
+        .reportper          = NRF_QDEC_REPORTPER_10,                 
+        .sampleper          = NRF_QDEC_SAMPLEPER_4096us,            
         .psela              = pin_a,                                
         .pselb              = pin_b,                                
-        .pselled            = pin_led,                              
-        .ledpre             = 500,                                   
-        .ledpol             = NRF_QDEC_LEPOL_ACTIVE_HIGH,            
+        .pselled            = NRF_QDEC_LED_NOT_CONNECTED,                                          
         .dbfen              = NRF_QDEC_DBFEN_DISABLE,                
         .sample_inten       = false,                                 
-        .interrupt_priority = NRFX_QDEC_DEFAULT_CONFIG_IRQ_PRIORITY  
+        .interrupt_priority = NRFX_QDEC_DEFAULT_CONFIG_IRQ_PRIORITY
 };
 
+    nrfx_err_t err;
 	
-	IRQ_CONNECT(QDEC_IRQn, 0, nrfx_isr, nrfx_qdec_irq_handler, 0);
-	irq_enable(QDEC_IRQn);
-	
-    
     err = nrfx_qdec_init(&qdec_config, qdec_nrfx_event_handler);
 	if (err == NRFX_SUCCESS) {
-		printk("OK");
+		printk("QDEC initialization was successful.\n");
 	}
-	else {printk("mordi");}
+    else if (err == NRFX_ERROR_INVALID_STATE) {
+        printk("QDEC was already initialized.");
+    }
+	else {printk("QDEC initialization failed.\n");}
 
     nrfx_qdec_enable();
 
+
     printk("Running...\n");
+
 
     while(1)
 	{
