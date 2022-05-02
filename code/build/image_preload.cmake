437722832fba414faba1288348ccef5e76ab04e2
Anders Kristensen <71840284+anderszk@users.noreply.github.com>
2022-05-02 14:27:50 +0200

    Delete code/build directory

--- code/build/image_preload.cmake
+++
@@ -1,8 +0,0 @@
-# Generated file that can be used to preload variant images
-set(BOARD nrf52840dk_nrf52840 CACHE INTERNAL "NCS child image controlled")
-set(CACHED_CONF_FILE C:/nsc_skd_1_9_1/applications/code/prj.conf CACHE INTERNAL "NCS child image controlled")
-set(CONFIG_DEBUG_OPTIMIZATIONS y CACHE INTERNAL "NCS child image controlled")
-set(CONFIG_DEBUG_THREAD_INFO y CACHE INTERNAL "NCS child image controlled")
-set(DTC_OVERLAY_FILE  CACHE INTERNAL "NCS child image controlled")
-set(NCS_TOOLCHAIN_VERSION NONE CACHE INTERNAL "NCS child image controlled")
-set(WEST_PYTHON c:\nsc_skd_1_9_1\v1.9.1\toolchain\opt\bin\python.exe CACHE INTERNAL "NCS child image controlled")
